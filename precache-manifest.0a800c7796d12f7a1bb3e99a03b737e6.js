eaa6cd670a17cc5559cabdac5679dd31eacd05b2
becover <sedi.september@gmail.com>
2020-01-03 00:03:45 +0900

    Updates

---
+++ precache-manifest.0a800c7796d12f7a1bb3e99a03b737e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a449b65c684ba30e4e0297db044ed93",
+    "url": "/movie-app-2019/index.html"
+  },
+  {
+    "revision": "2a2f23597f542c2d6686",
+    "url": "/movie-app-2019/static/css/main.ab05df2e.chunk.css"
+  },
+  {
+    "revision": "6380d79c66fff82ba313",
+    "url": "/movie-app-2019/static/js/2.3241c2aa.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/movie-app-2019/static/js/2.3241c2aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a2f23597f542c2d6686",
+    "url": "/movie-app-2019/static/js/main.fbc7c8f5.chunk.js"
+  },
+  {
+    "revision": "d2c0d5821df0102c306c",
+    "url": "/movie-app-2019/static/js/runtime-main.518e253b.js"
+  }
+]);
